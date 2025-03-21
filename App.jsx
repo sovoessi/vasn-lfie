dfb5469e0185e6fbf13cf645268a5394584cd50e
Jacques Sovoessi <jacques.sovoessi@gmail.com>
2025-03-21 12:05:31 +0100

    added vans page

--- App.jsx
+++ App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans"
+
+import "./server"
 
 function App() {
   return (
@@ -13,11 +16,13 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ pages/Vans.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Vans = () => {
+  return (
+    <h1>Vans page goes here 🚐</h1>
+  )
+}
+
+export default Vans
