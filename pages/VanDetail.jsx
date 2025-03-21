85469f1870fb8a4e387d686fbf1cfe95f22c053c
Jacques Sovoessi <jacques.sovoessi@gmail.com>
2025-03-21 16:46:20 +0100

    updated vandetail page

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
 
-const VanDetail = ({ van }) => {
+const VanDetail = () => {
   const params = useParams();
-  console.log(params);
+  const [van, setVan] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
 
   return (
-    <div className="van-tile">
-      <Link to={`/vans/${van.id}`}>
-        <img src={van.imageUrl} />
-        <div className="van-info">
-          <h3>{van.name}</h3>
-          <p>
-            ${van.price}
-            <span>/day</span>
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
           </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
         </div>
-        <i className={`van-type ${van.type} selected`}>{van.type}</i>
-      </Link>
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
   );
 };
--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import VanDetail from "./VanDetail";
+import {Link} from "react-router-dom";
 
 const Vans = () => {
   const [vans, setVans] = useState([]);
@@ -9,14 +9,26 @@ const Vans = () => {
       const response = await fetch("/api/vans");
       const data = await response.json();
       console.log(data.vans);
-      
+
       setVans(data.vans);
     };
     getVansList();
   }, []);
 
   const vanElements = vans.map((van) => (
-    <VanDetail key={van.id} van={van} />
+    <div key={van.id} className="van-tile">
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
+    </div>
   ));
 
   return (
