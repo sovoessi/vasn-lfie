ab4863cf5df28463c217d41c7afe3c5b92f02d2c
Jacques Sovoessi <jacques.sovoessi@gmail.com>
2025-03-21 15:17:54 +0100

    added VanDetail

--- App.jsx
+++ App.jsx
@@ -23,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
